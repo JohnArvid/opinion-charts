80b213041c4fc25498e3b300f3ca1c1cb1feeb64
Arvid Lindström <arvid-lindstrom@live.se>
2023-11-13 15:42:49 +0100

    More work on addLogo function

--- addLogo.js
+++ addLogo.js
@@ -11,11 +11,43 @@ function addLogo() {
   let nodes = document.querySelectorAll('.chart');
   
   
+  // for (let node of nodes) {
+  //   // let div = document.createElement('div');
+  //   let logo = document.createElement('img');
+  //   logo.setAttribute('src', src);
+  //   logo.setAttribute('style', css);
+  //   node.prepend(logo);
+  // }
   for (let node of nodes) {
-    // let div = document.createElement('div');
-    let logo = document.createElement('img');
-    logo.setAttribute('src', src);
-    logo.setAttribute('style', css);
-    node.prepend(logo);
+  // let div = document.createElement('div');
+  let logo = document.createElement('img');
+  logo.setAttribute('src', src);
+  logo.setAttribute('style', css);
+  if (node.classList.contains("table")) {
+    node.append(logo);
+  } else {
+  node.prepend(logo);
   }
 }
+}
+
+
+
+
+// container div css: 
+// {
+//     display: flex;
+//     flex-flow: column;
+//     align-items: end;
+//     margin: 0px 12%;
+//     position: relative;
+//     top: 16%;
+// }
+
+// img style: 
+  // {
+  //   z-index: 10000;
+  //   max-width: 145px;
+  // }
+
+
--- scripts.js
+++ scripts.js
@@ -140,7 +140,7 @@ function drawCharts() {
   drawLines(ministerData, 'lineChartByMinister', 'Trend per statsministerkandidat');
   drawTable(partyData, 'table', true);
   drawTable(partyData, 'table2', false);
-  // addLogo();
+  addLogo();
 }
 
 // Listen for window resize and redraw charts 
@@ -381,3 +381,6 @@ function drawTable(dataObject, chartId, transposeTable) {
   
   table.draw(data, options);
 }
+
+
+
