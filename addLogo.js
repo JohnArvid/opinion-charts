6d9226847732a84f54643daf3b2da4e03a79eb4d
JohnArvid <arvid-lindstrom@live.se>
2023-11-20 15:12:54 +0100

    Fixed addLogo function

--- addLogo.js
+++ addLogo.js
@@ -1,53 +1,26 @@
 
-/*  Needs some work, 
-    logos overlap some charts
-    becomes very big in fullwidth charts
-    should be appended instead of prepended
-    in tables.
-*/
+
+
 function addLogo() {
-  let css = 'position: relative; top: 16%; width: 17%; z-index: 10000; right: -67%; max-width: 145px;'
-  let src = 'https://www.indikator.org/wp-content/uploads/2023/06/Logo_temp.png'
+  let css = "display: flex; flex-flow: column; align-items: end; margin: 0px 12%; position: relative; top: 16%;";
+  let tableCss = "display: flex; flex-flow: column; align-items: end; margin: 0; position: relative; top: 0;";
+  let src = 'https://www.indikator.org/wp-content/uploads/2023/06/Logo_temp.png';
   let nodes = document.querySelectorAll('.chart');
   
   
-  // for (let node of nodes) {
-  //   // let div = document.createElement('div');
-  //   let logo = document.createElement('img');
-  //   logo.setAttribute('src', src);
-  //   logo.setAttribute('style', css);
-  //   node.prepend(logo);
-  // }
+ 
   for (let node of nodes) {
-  // let div = document.createElement('div');
-  let logo = document.createElement('img');
-  logo.setAttribute('src', src);
-  logo.setAttribute('style', css);
-  if (node.classList.contains("table")) {
-    node.append(logo);
-  } else {
-  node.prepend(logo);
+    let div = document.createElement('div');
+
+    div.innerHTML = `<img src=${src} style='z-index: 10000; width: 145px; max-width: 20%;'/>`
+
+    if (node.classList.contains("table")) {
+      div.setAttribute("style", tableCss);
+      node.prepend(div);
+    } else {
+      div.setAttribute("style", css);
+      node.prepend(div);
+    }
   }
 }
-}
-
-
-
-
-// container div css: 
-// {
-//     display: flex;
-//     flex-flow: column;
-//     align-items: end;
-//     margin: 0px 12%;
-//     position: relative;
-//     top: 16%;
-// }
-
-// img style: 
-  // {
-  //   z-index: 10000;
-  //   max-width: 145px;
-  // }
-
 
