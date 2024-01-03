1ef363c533dd8fcf737f2fb1bbd86412bdf53ef8
JohnArvid <arvid-lindstrom@live.se>
2024-01-03 15:15:59 +0100

    integrated data-wash

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,53 @@
+let dataAsString  = 'December (2023)\t7,2\t5,3\t37,0\t3,4\t2,7\t17,5\t2,6\t23,0\t1,4\nNovember (2023)\t7,3\t4,3\t38,3\t3,8\t3,2\t16,2\t2,7\t22,6\t1,6\nOktober (2023)\t8,1\t4,8\t37,9\t4,1\t2,3\t16,1\t2,9\t22,0\t1,8\nSeptember (2023)\t7,6\t5,2\t38,6\t4,0\t3,0\t18,4\t2,6\t19,0\t1,5\nJuni (2023)\t7,8\t5,0\t36,6\t4,3\t3,0\t18,5\t4,1\t18,3\t2,4\nValresultatet 2022\t6,8\t5,1\t30,3\t6,7\t4,6\t19,1\t5,3\t20,5\t1,5\n';
+let blockDataAsString = 'December (2023)\t45,8\t52,9\nNovember (2023)\t44,7\t53,7\nOktober (2023)\t43,3\t54,9\nSeptember (2023)\t43\t55,4\nJuni (2023)\t43,9\t53,7\nValet 2022\t49,5\t48,9\n'; 
+let ministerDataAsString = 'December (2023)\t44\t18\t18\t20\nNovember (2023)\t43\t17\t18\t22\nOktober (2023)\t45\t18\t17\t20\nSeptember (2023)\t48\t22\t13\t17\n';
+// Utility for table
+function createDataArrayFromString(stringData){
+  // gör generell tvätt först:
+  let replaced = stringData.replaceAll(',', '.').replaceAll('\t', ', ');
 
+  if (replaced.indexOf('\n') > 0) {
+    let arrOfStrings = replaced.split('\n');
+    let arrOfArrs = arrOfStrings.map( item => item.split(', '));
+    if (arrOfArrs[arrOfArrs.length -1].length == 1) arrOfArrs.pop();
+    return arrOfArrs.map( array => convertItemsToNumber(array));
+  }
+
+  return convertItemsToNumber(replaced.split(', '));
+
+}
+
+
+function convertItemsToNumber(arr) {
+  let mapped = arr.map( (item, index) => { if ( isNaN(item) ) {
+    return item 
+  } else {
+    return +item
+    }
+  });
+  return mapped;
+}
+
+// Utility for single columns
+function createDataArrayFromStringVertical(stringData){
+  let replaced = stringData.replaceAll(',', '.').replaceAll('\n', ', ');
+  let asArray = replaced.split(', ');
+  if (asArray[asArray.length -1] == '') asArray.pop();
+  return asArray.map( (item, index) => { if ( isNaN(item) ) {
+    return item 
+  } else {
+    return +item
+    }
+  });
+}
+
+
+const currentData = createDataArrayFromString(dataAsString);
+const currentBlockData = createDataArrayFromString(blockDataAsString);
+const currentMinisterData = createDataArrayFromString(ministerDataAsString);
 
 const partyData = {
-  results: [ 
-    8.1, 
-    4.2,  
-    37.9,  
-    4.1,  
-    2.3,  
-    16.1,  
-    2.9, 
-    22.0,  
-    1.8, ],
+  results: currentData[0].slice(1),
   seriesNames:  [
     'V',  
     'MP',
@@ -43,53 +80,43 @@ const partyData = {
     sd: '#e2b404',
     ovr:  '#808080',
   },
-  changeSinceLast: [
-    0.5,
-    -0.4, 
-    -0.7,
-    0.1,
-    -0.7,
-    -2.3,
-    0.3,
-    3.0,
-    0.3 ],
-    changeSinceElection: [
-    1.3,
-    -0.3, 
-    7.6,
-    -2.6,
-    -2.3,
-    -3.0,
-    -2.4,
-    1.5,
-    0.3 ],
-  month: "Oktober",
-  year: 2023,
-  history: [
-    ['Valresultatet 2022',6.8,5.1,30.3,6.7,4.6,19.1,5.3,20.5,1.5],
-    ['Juni (2023)', 7.8,5.0,36.6,4.3,3.0,18.5,4.1,18.3,2.4],
-    ['September (2023)',7.6,5.2,38.6,4.0,3.0,18.4,2.6,19.0,1.5],
-    ['Oktober (2023)',8.1,4.8,37.9,4.1,2.3,16.1,2.9,22.0,1.8],
-  ],
+  changeSinceLast: createDataArrayFromStringVertical(`-0,1
+1,0
+-1,3
+-0,4
+-0,5
+1,3
+-0,1
+0,4
+-0,2
+`),
+    changeSinceElection: createDataArrayFromStringVertical(`0,4
+0,2
+6,7
+-3,3
+-1,9
+-1,6
+-2,7
+2,5
+-0,1
+`),
+  month: currentData[0][0].slice(0, currentData[0][0].length - 7),
+  year: currentData[0][0].slice(currentData[0][0].length - 5, currentData[0][0].length - 1),
+  history: currentData.toReversed(),
 }
 
 const blockData = {
-  results: [ 43.3, 54.9 ], 
+  results: currentBlockData[0].slice(1), 
   seriesNames:  [ 'M/L/KD/SD', 'S/V/MP/C', ],
   seriesColors : {
     m:  partyData.seriesColors.m,
     s:  partyData.seriesColors.s,
   },
-  history: [
-    ['Valresultatet 2022', 49.5, 48.9],
-    ['Juni (2023)', 43.9, 53.7],
-    ['September (2023)', 43.0, 55.4],
-    ['Oktober (2023)', 43.3, 54.9],
-  ],
+  history: currentBlockData.toReversed(),
 }
 
 const ministerData = {
-  results: [45,  18,  17,  20],
+  results: currentMinisterData[0].slice(1),
   seriesNames: [
     'Magdalena Andersson', 
     'Ulf Kristersson', 
@@ -102,10 +129,7 @@ const ministerData = {
     sd: partyData.seriesColors.sd,
     ovr: partyData.seriesColors.ovr,
   },
-  history: [
-    ['September (2023)',  48,  22,  13,  17],
-    ['Oktober (2023)',  45,  18,  17,  20],
-    ],
+  history: currentMinisterData.toReversed(),
 }
 
 
@@ -387,7 +411,7 @@ function drawLines(dataObject, chartId, title = "") {
     dataObject.seriesNames)],
   ...dataObject.history
   ]);
-  if (dataObject == ministerData) console.log(data);
+  // if (dataObject == ministerData) console.log(data);
 
   const chart = new google.visualization.LineChart(
     document.getElementById(chartId)
